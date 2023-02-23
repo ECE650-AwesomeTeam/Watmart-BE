a550960edece53e6eab13d0dc0fb807f95c6e122
Xuan Shi <xuans1999@gmail.com>
2023-02-23 16:50:01 -0500

    add posting product function

--- watmart/urls.py
+++ watmart/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from backbone import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup', views.signup)
+    path('signup', views.signup),
+    path('post', views.create_post),
+    path('post/<product_id>', views.update_post)
 ]
