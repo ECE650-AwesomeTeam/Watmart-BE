e59cb10812bd51fd7d48131674f0b293bc9e8b0c
Mustafa K <122950297+MisterRushB@users.noreply.github.com>
2023-03-05 10:58:10 -0500

    Update views.py
    Added a POST method for User model

--- backbone/api/views.py
+++ backbone/api/views.py
@@ -5,4 +5,14 @@ from rest_framework.decorators import api_view
 from backbone.models import User 
 from backbone.api.serializers import UserSerializer
 
-#Continue from here
+@api_view(['POST',]) 
+def api_detail_User(request):
+  
+  if request.method=="POST": 
+    serializer=UserSerializer(data=request.data) 
+    data={} 
+    if serializer.is_valid(): 
+      serializer.save()
+      return Response(serializer.data,status.HTTP_201_CREATE) 
+    return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
+  
