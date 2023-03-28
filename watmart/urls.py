bdfeca356567f515cc030143c400968de6f51b1f
Xuan Shi <xuans1999@gmail.com>
2023-03-27 22:05:48 -0400

    make media folder accessiable

--- watmart/urls.py
+++ watmart/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.urls import re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from backbone import views
 
 urlpatterns = [
@@ -26,5 +27,6 @@ urlpatterns = [
     path('login', views.login),
     path('post', views.create_post),
     path('post/<product_id>', views.update_post),
-    re_path(r'^post/$', views.get_post)
+    re_path(r'^post/$', views.get_post),
+    re_path(r'^media/(?P<path>.*)', serve, {'document_root': settings.MEDIA_ROOT})
 ]
